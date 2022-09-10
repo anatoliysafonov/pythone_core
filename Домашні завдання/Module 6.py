30b40bde0721268fcc15ac84bc16ada70a556b5d
anatoliysafonov <anatoliy.safonov.ua@gmail.com>
2022-09-10 07:44:53 +0300

    10.09 Update

--- Домашні завдання/Module 6.py
+++ Домашні завдання/Module 6.py
@@ -21,11 +21,11 @@ LIST_OF_FOLDERS = {'pictures':     ['JPEG', 'PNG', 'JPG', 'SVG'],
 
 
 def normalize_name(name) -> str:
-    TRANS = {}
+    TRANS_DICT = {}
     for i, j in zip(CYRILLIC_SYMBOLS, TRANSLATION):
-        TRANS[ord(i)] = j
-        TRANS[ord(i.upper())] = j.upper()
-    asc2_name = name.translate(TRANS)
+        TRANS_DICT[ord(i)] = j
+        TRANS_DICT[ord(i.upper())] = j.upper()
+    asc2_name = name.translate(TRANS_DICT)
     normalized_name = ''
     for char in asc2_name:
         if char in string.punctuation:
@@ -42,61 +42,74 @@ def check_folders_exists():
         if not os.path.isdir(PATH_TO_SORTED_DIRECTORY + folder_name):
             os.mkdir(PATH_TO_SORTED_DIRECTORY + folder_name)
 
+ # функція  яка шукає і повертає рядок, який в вказує на тип файла та на каталог в який знайдений файл буде переміщений
 
-def sort_files_in_directory(path):
 
-    # функція  яка шукає і повертає рядок, який в вказує на тип файла та на каталог в який знайдений файл буде переміщений
-    def find_type_of_file(ext):
-        ext = ext[1:]
-        type_founded = None
-        for type_of_file in LIST_OF_FOLDERS:
-            if ext.upper() in LIST_OF_FOLDERS[type_of_file]:
-                type_founded = str(type_of_file)
-        if not type_founded:
-            type_founded = 'others'
-        return type_founded
+def find_type_of_file(extention: str) -> str:
+    extention = extention[1:]
+    founded: str = None
+    for type_of_file in LIST_OF_FOLDERS:
+        if extention.upper() in LIST_OF_FOLDERS[type_of_file]:
+            founded = str(type_of_file)
+    if not founded:
+        founded = 'others'
+    return founded
 
+
+def sort_files_in_directory(source_path) -> bool:
     # шукаємо все, що є в каталогу (файли + вкладені каталоги)
-    list_of_files_in_directory = os.listdir(path)
+    list_of_files_in_directory = os.listdir(source_path)
 
-    for file in list_of_files_in_directory:
-        full_path = path + file
+    # якщо папка пуста, виходимо з программи
+    if not list_of_files_in_directory:
+        return True
+
+    for current_item in list_of_files_in_directory:
+        full_path_current_item = source_path + current_item
         # блок якщо в нас файл
-        if os.path.isfile(full_path):
+        if os.path.isfile(full_path_current_item):
             # отримуємо окремо назву файла та його розширення
-            name_file = os.path.splitext(file)[0]
-            ext = os.path.splitext(file)[1]
+            name_current_item = os.path.splitext(current_item)[0]
+            extention_current_item = os.path.splitext(current_item)[1]
 
             # будуємо шлях до новго каталога на основі типу файла якого ми переміщаємо
-            new_full_path = PATH_TO_SORTED_DIRECTORY + \
-                find_type_of_file(ext) + '/'
+            destination_path_current_item = PATH_TO_SORTED_DIRECTORY + \
+                find_type_of_file(extention_current_item) + '/'
 
             # нормалізуємо імʼя файла
             name_of_file_normalized = normalize_name(
-                name_file) + ext
+                name_current_item) + extention_current_item
 
             # якщо файл це архів, то розрхівовуємо його в папці для архівів
-            if ext[1:].upper() in LIST_OF_FOLDERS['archives']:
-                os.mkdir(new_full_path + normalize_name(name_file))
-                shutil.unpack_archive(
-                    path + file, new_full_path + normalize_name(name_file))
-                os.remove(path + file)
+            if extention_current_item[1:].upper() in LIST_OF_FOLDERS['archives']:
+                if not os.path.exists(destination_path_current_item + normalize_name(name_current_item)):
+                    os.mkdir(destination_path_current_item +
+                             normalize_name(name_current_item))
+                try:
+                    shutil.unpack_archive(
+                        source_path + current_item, destination_path_current_item + normalize_name(name_current_item))
+                except shutil.ReadError:
+                    print('{} архів не є архівом, або він пошкоджений'.format(
+                        current_item))
+                else:
+                    os.remove(source_path + current_item)
                 continue
 
             # переносимо файл з нормалізованою назвою в папку для файлів такого типу
-            print(PATH_TO_UNSORTED_DIRECTORY + file)
-            print(new_full_path + name_of_file_normalized)
-            os.replace(path + file,
-                       new_full_path + name_of_file_normalized)
+            os.replace(source_path + current_item,
+                       destination_path_current_item + name_of_file_normalized)
 
-        # якщо в нас каталог, то ми рекурсивно викликаємо функцію soft_files_in_dictionary і передаємо знайдений каталог як агрумент
-        if os.path.isdir(full_path):
-            print(full_path)
-            sort_files_in_directory(full_path + '/')
-            shutil.rmtree(full_path)
-    return
+        # якщо в нас каталог, то ми рекурсивно викликаємо функцію sort_files_in_dictionary і передаємо знайдений каталог як агрумент
+        if os.path.isdir(full_path_current_item):
+            print(full_path_current_item)
+            sort_files_in_directory(full_path_current_item + '/')
+            shutil.rmtree(full_path_current_item)
+    return True
 
 
 if __name__ == '__main__':
     check_folders_exists()
-    sort_files_in_directory(PATH_TO_UNSORTED_DIRECTORY)
+    if sort_files_in_directory(PATH_TO_UNSORTED_DIRECTORY):
+        print('Done')
+
+# Продивитись else ;
