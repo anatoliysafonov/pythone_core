3a80337e7faea90ca892b5abdf65f6a6f9277722
anatoliysafonov <anatoliy.safonov.ua@gmail.com>
2022-09-14 13:53:34 +0300

    update 12th task

---
+++ Домашні завдання/Python Core/Hw07/12/12.py
@@ -0,0 +1,11 @@
+def get_employees_by_profession(path, profession):
+    read_data = None
+    output_data = []
+    with open(path, 'r') as file:
+        read_data = file.readlines()
+    for item in range(len(read_data)):
+        if read_data[item].find(profession) != -1:
+            name = read_data[item].replace(profession, '')[:-1].strip()
+            output_data.append(name)
+
+    return ' '.join(output_data)
