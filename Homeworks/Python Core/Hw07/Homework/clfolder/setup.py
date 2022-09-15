b76e59d4de94ead365d098ae9e363004ae238050
anatoliysafonov <anatoliy.safonov.ua@gmail.com>
2022-09-15 12:42:12 +0300

    added some text

--- Homeworks/Python Core/Hw07/Homework/clfolder/setup.py
+++ Homeworks/Python Core/Hw07/Homework/clfolder/setup.py
@@ -4,9 +4,9 @@ setup(
     name='clean_folder',
     version='1',
     description='User for clean any folder',
-    url='',
+    url='https://github.com/anatoliysafonov/pythone_core/blob/main/Homeworks/Python%20Core/Hw07/Homework/clfolder/clfolder/clean.py',
     author='anatoliy safonov',
-    author_email='',
+    author_email='anatoliy.safonov.ua@gmail.com',
     license='MIT',
     packages='',
     entry_points={'console_scripts': [
